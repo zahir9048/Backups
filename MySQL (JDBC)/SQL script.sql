575bd6eedec0b5f176f1e9610a86353a004575f2
zahir9048 <78537600+zahir9048@users.noreply.github.com>
2023-08-31 22:08:47 +0500

    Update SQL script.sql

--- MySQL (JDBC)/SQL script.sql
+++ MySQL (JDBC)/SQL script.sql
@@ -20,3 +20,13 @@ INNER JOIN customers c
 ON c.order_id = o.order_id
 WHERE c.name = 'darren'
 ORDER BY c.name;
+
+# 2nd Method
+SELECT DISTINCT c.name,
+    (SELECT p.name FROM products p, orders o WHERE p.product_id = 1 AND o.product_id = p.product_id LIMIT 1) AS product_name
+FROM customers c
+WHERE c.order_id IN (
+    SELECT o.order_id
+    FROM orders o
+    WHERE o.product_id = 1
+);
